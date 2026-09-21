d5b81d1a2de644f354369a0f5595270bb8c02e23
AbuuRasel <jraselahmad@gmail.com>
2026-09-21 17:41:51 +0600

    profiles: balanced input boost 40 -> 80ms for scroll smoothness
    Covers scroll flings (FB/YT) without raising touch floors (heat).
    Devfreq 200ms input boost covers the bus.

--- drivers/misc/neurocore_profile.c
+++ drivers/misc/neurocore_profile.c
@@ -72,7 +72,9 @@ static void neuro_apply_profile(int p)
 	case NEURO_BALANCED:
 	default:
 #ifdef CONFIG_CPU_BOOST
-		cpuboost_set_input_boost_ms(40);
+		/* 80ms covers scroll flings (FB/YT) without raising touch
+		 * floors (heat). Devfreq 200ms input boost covers the bus. */
+		cpuboost_set_input_boost_ms(80);
 		cpuboost_set_boost_freq(1363200, 1401600);
 		cpuboost_set_sched_boost_on_input(0);
 #endif
